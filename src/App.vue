0199c1afae42ee8020d1db6174d4bd11127c9fed
Mohammad Arshad <arshad.comp@gmail.com>
2024-10-05 19:39:09 +0530

    New production branch with vue3

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,10 @@
+<script setup lang="ts">
+import Todos from './components/Todos.vue'
+</script>
+
 <template>
-    <b-container id="app" class="p-0 m-0 bg-light" fluid>
-      <Menu/>
-      <router-view></router-view>
-      <Footer/>
-    </b-container>
+  <main>
+    <Todos />
+  </main>
 </template>
 
-<script>
-import { Auth } from 'aws-amplify'
-import { onAuthUIStateChange } from '@aws-amplify/ui-components'
-
-import Menu from "@/components/Menu.vue";
-import Footer from "@/components/Footer.vue"
-
-export default {
-  name: 'App',
-  components: {
-    Menu,
-    Footer,
-  },
-  created() {
-    Auth.currentAuthenticatedUser()
-      .then((user) => {
-        this.$store.commit('SET_AUTH_STATE', 'signedin')
-        this.$store.commit('SET_AUTH_DATA', {attributes: user.attributes, signInUserSession: user.signInUserSession})
-      }).catch(err => {
-        console.log(err)
-        this.$store.commit('SET_AUTH_STATE', 'signedout')
-        this.$store.commit('SET_AUTH_DATA', null)
-      })
-
-    onAuthUIStateChange((authState, authData) => {
-      console.log('AUTH STATE', authState)
-      if(authState === 'signedin') {
-        this.$store.commit('SET_AUTH_STATE', authState)
-        this.$store.commit('SET_AUTH_DATA', authData)
-      } else {
-        this.$store.commit('SET_AUTH_STATE', 'signedout')
-        this.$store.commit('SET_AUTH_DATA', null)
-      }
-    })
-  },
-  beforeDestroy() {
-    return onAuthUIStateChange;
-  }
-}
-</script>
